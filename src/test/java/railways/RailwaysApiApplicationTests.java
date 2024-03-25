14b460e592e750906bfde9b7484ac20356c75004
skylinedude <manoj1999gtr@gmail.com>
2024-03-25 20:20:39 +0530

    'deployment'

---
+++ src/test/java/railways/RailwaysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package railways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
